780b897bf8eb2bc6f9fb419b9868368a9f109037
Xm3Ga <adripbahamont@gmail.com>
2024-07-05 15:13:35 +0200

    Primera refactorización (y espero que última) de la clase Hamburguesa y cambios al readme

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ Dejaremos atrás los arrays (al menos parcialmente) y procederemos a refactoriza
 
 ### Solución (TODO)
 
-Modificamos la clase hamburguesa para que utilize un List<Ingrediente> ingredientes. Esto nos va a permitir mucha más libertad a la hora de añadir los ingredientes y solo hace faltan unos pocos metodos adicionales para completar el CRUD. 
+Modificamos la clase hamburguesa para que utilize un List<Ingrediente> ingredientes. Esto nos va a permitir mucha más libertad a la hora de añadir los ingredientes y solo hace faltan unos pocos metodos adicionales para completar el CRUD. Cambiamos también el constructor de la clase para que la hamburguesa, en vez de tener una estructura fija, sea un ArrayList de ingredientes, esto permite poner cualquier ingrediente en cualquier posición, además de libertad de tamaño.
+
+TODO
+
+- Modificar el resto de clases para que reflejen los cambios de Hamburguesa
+
+- Realizar una implementación del CRUD en Cocinero para comprobar que todo funcione bien y el ejercicio esté completado
--- src/Hamburguesa.java
+++ src/Hamburguesa.java
@@ -1,39 +1,47 @@
 package src;
 
-class Hamburguesa {
-    private Pan[] panes;
-    private Carne carne;
-    private Extra[] extras;
-    private int extrasAgregados;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Hamburguesa(Pan panSuperior, Pan panInferior, Carne carne, int numeroExtras) {
-        this.panes = new Pan[]{panSuperior, panInferior};
-        this.carne = carne;
-        this.extras = new Extra[numeroExtras];
-        this.extrasAgregados = 0;
+class Hamburguesa {
+    private List<Ingrediente> ingredientes;
+
+    public Hamburguesa() {
+        this.ingredientes = new ArrayList<>();
     }
 
-    public void agregarExtra(Extra extra) {
-            extras[extrasAgregados] = extra;
-            extrasAgregados++;
+    public void agregarIngrediente(Ingrediente ingrediente) {
+        ingredientes.add(ingrediente);
+    }
+
+    public void agregarIngrediente(Ingrediente ingrediente, int posicion) {
+        ingredientes.add(posicion, ingrediente);
+    }
+
+    public void quitarIngrediente(int posicion) {
+        ingredientes.remove(posicion);
+    }
+
+    public void reorganizarIngredientes(int desdePos, int hastaPos) {
+        Ingrediente ingrediente = ingredientes.remove(desdePos);
+        ingredientes.add(hastaPos, ingrediente);
     }
 
     public void mostrar() {
-        panes[0].mostrar();
-        carne.mostrar();
-        for (int i = 0; i < extrasAgregados; i++) {
-            extras[i].mostrar();
+        for (Ingrediente ingrediente : ingredientes) {
+            ingrediente.mostrar();
         }
-        panes[1].mostrar();
     }
 
-    public String describir() {   
-        String descripcion; 
-        descripcion = panes[0].describir() + ", " + carne.describir();
-        for (int i = 0; i < extrasAgregados; i++) {
-            descripcion = descripcion + ", " + extras[i].describir();
+    public String describir() {
+        StringBuilder descripcion = new StringBuilder();
+        for (int i = 0; i < ingredientes.size(); i++) {
+            descripcion.append(ingredientes.get(i).describir());
+            if (i < ingredientes.size() - 1) {
+                descripcion.append(", ");
+            }
         }
-        descripcion = descripcion + "\n";
-        return descripcion;
+        descripcion.append("\n");
+        return descripcion.toString();
     }
 }
